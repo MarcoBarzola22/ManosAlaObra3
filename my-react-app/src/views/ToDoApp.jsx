850181dbe2aedb7d20bbb243f3d5a391323e5cde
MarcoDev <marcobarzoladev@gmail.com>
2025-09-09 20:00:46 -0300

    Agregando eventos (useState) en ToDoApp.jsx

--- my-react-app/src/views/ToDoApp.jsx
+++ my-react-app/src/views/ToDoApp.jsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import "../styles/App.css";
 import Input from "../components/Input/Index";
 import Button from "../components/Button";
 import TaskList from "../components/TaskList";
 
 export default function ToDoApp() {
+    //estado de la lista de tareas
+    const [tasks, setTasks] = useState([]);
+
+    //estado del input
+    const [newTask, setNeTask] = useState("");
+
+    function handleInputChange(evente){
+        setNewTask(evente.target.value);
+    }
+
     return (
         <div className="container">
             <h1>LISTA DE TAREAS DE MARCO</h1>
 
             <div className="add-task">
-                <Input placeholder="Nueva Tarea..." /> 
+                <Input placeholder="Nueva Tarea..." value={newTask} onChange={handleInputChange}/> 
                 <Button>ADD</Button> 
             </div> 
 
-            <TaskList />
+            <TaskList task={tasks}/>
         </div>
     );
 }
